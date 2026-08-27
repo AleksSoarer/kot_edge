ebdd942c276714502f7a8516c14cd89a307153f6
Лукишин Александр <alukishin@itcontour.ru>
2026-08-27 22:31:42 +0700

    Flip Kot reply pointer layout

--- kot/main.py
+++ kot/main.py
@@ -267,7 +267,7 @@ async def lifespan(_: FastAPI):
                 await task
 
 
-app = FastAPI(title="Kot Edge", version="0.8.6", lifespan=lifespan)
+app = FastAPI(title="Kot Edge", version="0.8.7", lifespan=lifespan)
 app.mount("/static", StaticFiles(directory=STATIC_DIR), name="static")
 
 
